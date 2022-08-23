e41072295c5396804a71909c1e8c64e9bd318d21
spellcry <sangiothebest@hotmail.com>
2022-08-23 14:48:25 +0200

    add image

---
+++ bonus/js/scripts.js
@@ -0,0 +1,9 @@
+const app = new Vue({
+    el: '#main-content',
+    data: {
+        name: 'Vue Hello App',
+        titolo: 'Vue Hello App',
+        imageSrc: 'https://picsum.photos/500/300',
+        imageAlt: 'Immagine casuale da LoremPicsum',
+    },
+});
