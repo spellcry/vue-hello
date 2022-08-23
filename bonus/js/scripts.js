e8503b5dfa797cd05008bbbeea0d8b60e8b5fc67
spellcry <sangiothebest@hotmail.com>
2022-08-23 17:30:32 +0200

    add modify image dimensions

--- bonus/js/scripts.js
+++ bonus/js/scripts.js
@@ -2,8 +2,26 @@ const app = new Vue({
     el: '#main-content',
     data: {
         name: 'Vue Hello App',
+        containerClass: 'container flex',
         titolo: 'Vue Hello App',
-        imageSrc: 'https://picsum.photos/500/300',
+        imageSrc: 'https://picsum.photos/',
+        imageHeight: '300',
+        imageWidth: '500',
         imageAlt: 'Immagine casuale da LoremPicsum',
     },
+    methods: {
+        toUpperCase(text) {
+            return text.toUpperCase();
+        },
+        clickModifyImgDimensions() {
+            imgWidth = parseInt(document.getElementById('image-width').value);
+            imgHeight = parseInt(document.getElementById('image-height').value);
+            if (!isNaN(imgWidth)) {
+                this.imageWidth = imgWidth;
+            }
+            if (!isNaN(imgHeight)) {
+                this.imageHeight = imgHeight;
+            }
+        }
+    }
 });
